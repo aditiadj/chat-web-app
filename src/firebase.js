470c5b6dbe016488dca0c49ba897d70b0501480e
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-04-20 20:43:46 +0700

    Refactoring code

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase"
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  };
+
+  firebase.initializeApp(config);
+
+  const database = firebase.database()
+
+  export {database};
